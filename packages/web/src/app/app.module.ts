f148cc0dab17d0118a76c4122db3374edd3886c7
Lucas Polesello <lucas.c4d@gmail.com>
2022-04-03 19:40:59 -0300

    added bootstrap

--- packages/web/src/app/app.module.ts
+++ packages/web/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import {HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AlertComponent } from './alert/alert.component'
+import { AlertComponent } from './alert/alert.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -15,17 +16,16 @@ import { AlertComponent } from './alert/alert.component'
     MainPageComponent,
     LoginPageComponent,
     NavBarComponent,
-    AlertComponent
+    AlertComponent,
   ],
   imports: [
-     BrowserModule,
-      BrowserAnimationsModule,
-       ReactiveFormsModule,
-       HttpClientModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-
-}
+export class AppModule {}
